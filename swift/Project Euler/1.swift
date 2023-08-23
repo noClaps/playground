b1421efba0419e64b91fef091fb8233a57e3ef3d
noClaps <github@zerolimits.dev>
2023-08-23 17:39:25 +0800

    Update 1.swift

--- swift/Project Euler/1.swift
+++ swift/Project Euler/1.swift
@@ -1,13 +1,13 @@
 func sum3Or5(lim: Int) -> Int {
-    var sum = 0
-    
-    for n in 0..<lim {
-        if n % 3 == 0 || n % 5 == 0 {
-            sum += n
-        }
-    }
+  var sum = 0
 
-    return sum
+  for n in 0..<lim {
+    if n % 3 == 0 || n % 5 == 0 {
+      sum += n
+    }
+  }
+
+  return sum
 }
 
 let sumTo10 = sum3Or5(lim: 10)
