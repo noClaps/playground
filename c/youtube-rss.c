345cce4c83ed78b33b86d694213b4dc66a65cd65
noClaps <github@zerolimits.dev>
2023-10-23 20:04:22 +0100

    Use command line arguments for youtube-rss script

--- c/youtube-rss.c
+++ c/youtube-rss.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
 
-int main(void) {
-  // Declare variables
-  char channelId[24];
-
-  // Request input of channel ID
-  printf("Enter channel ID\n");
-  scanf("%s", channelId);
-
+int main(int argc, char *argv[]) {
   // Print channel RSS feed to console
   printf("Your channel's RSS feed is:\n");
-  printf("https://www.youtube.com/feeds/videos.xml?channel_id=");
-  puts(channelId);
+  printf("https://www.youtube.com/feeds/videos.xml?channel_id=%s\n", argv[1]);
 
   return 0;
 }
