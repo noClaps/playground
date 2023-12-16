effb273f4774ac9b41a0511fddec28f55417ec37
noClaps <github@zerolimits.dev>
2023-12-16 09:15:32 +0000

    Create hopper.swift

---
+++ swift/hopper.swift
@@ -0,0 +1,18 @@
+// You start at the beginning of an array (index=0). The value of the array at
+// that index is the jump distance. Determine if you can reach the end of the
+// array.
+
+let myArray = [2, 3, 1, 1, 4]
+var i = 0
+
+while true {
+  i += myArray[i]
+
+  if i > myArray.count || myArray[i] == 0 {
+    print(false)
+    break
+  } else if i == myArray.count - 1 {
+    print(true)
+    break
+  }
+}
