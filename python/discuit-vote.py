ded2824e0d1f646c56e502a8a759ecd2f2140559
noClaps <04plugs-bios@icloud.com>
2024-10-13 00:39:27 +0100

    style: Format Python code

--- python/discuit-vote.py
+++ python/discuit-vote.py
@@ -1,10 +1,12 @@
 def points(up: int, down: int):
-	return up - down
+    return up - down
+
 
 def percentage(up: int, down: int):
-	return round(up/(up+down) * 100)
+    return round(up / (up + down) * 100)
+
 
 for up in range(6001):
-	for down in range(6001):
-		if points(up, down) == 4 and percentage(up, down) == 84:
-			print(f"upvotes: {up}, downvotes: {down}")
+    for down in range(6001):
+        if points(up, down) == 4 and percentage(up, down) == 84:
+            print(f"upvotes: {up}, downvotes: {down}")
--- python/string-encoder.py
+++ python/string-encoder.py
@@ -1,51 +1,55 @@
 from itertools import permutations
 
+
 def perms(alphabet: list, length: int, type: list[float]):
-	counts = [int(t * length) for t in type]
-	string = "".join(str(alphabet[i]) * counts[i] for i in range(len(counts)))
-	perms = list(set("".join(p) for p in permutations(string)))
-	return sorted(perms)
+    counts = [int(t * length) for t in type]
+    string = "".join(str(alphabet[i]) * counts[i] for i in range(len(counts)))
+    perms = list(set("".join(p) for p in permutations(string)))
+    return sorted(perms)
+
 
 class Encoder:
-	alphabet: list
-	index: int
-	length: int
-	string: str
-	type: list[float]
+    alphabet: list
+    index: int
+    length: int
+    string: str
+    type: list[float]
 
-	def __init__(self, string: str):
-		self.string = string
-		self.length = len(self.string)
-		self.alphabet = list(set(string))
-		self.type = self.getType()
-		self.index = perms(self.alphabet, self.length, self.type).index(string)
+    def __init__(self, string: str):
+        self.string = string
+        self.length = len(self.string)
+        self.alphabet = list(set(string))
+        self.type = self.getType()
+        self.index = perms(self.alphabet, self.length, self.type).index(string)
 
-	def getType(self):
-		type: list[float] = []
-		for char in self.alphabet:
-			type.append(self.string.count(f"{char}") / self.length)
-		return type
+    def getType(self):
+        type: list[float] = []
+        for char in self.alphabet:
+            type.append(self.string.count(f"{char}") / self.length)
+        return type
 
-	def encode(self):
-		return self.alphabet, self.index, self.length, self.type
+    def encode(self):
+        return self.alphabet, self.index, self.length, self.type
+
 
 class Decoder:
-	alphabet: list[int]
-	index: int
-	length: int
-	type: list[float]
+    alphabet: list[int]
+    index: int
+    length: int
+    type: list[float]
 
-	def __init__(self, alphabet: list[int],index: int,length: int, type: list[float]):
-		self.alphabet = alphabet
-		self.length = length
-		self.index = index
-		self.type = type
+    def __init__(self, alphabet: list[int], index: int, length: int, type: list[float]):
+        self.alphabet = alphabet
+        self.length = length
+        self.index = index
+        self.type = type
 
-	def decode(self):
-		type_class = perms(self.alphabet, self.length, self.type)
-		return type_class[self.index]
+    def decode(self):
+        type_class = perms(self.alphabet, self.length, self.type)
+        return type_class[self.index]
 
-string = "the quick brown fox jumps over the lazy dog"
+
+string = "the quick"
 
 encoder = Encoder(string)
 alphabet, index, length, type = encoder.encode()
