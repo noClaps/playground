96e73a49e20b2028f9c3779e7af06b8f2dbd8522
noClaps <git@zerolimits.dev>
2026-01-16 21:03:11 +0000

    Rewrite fizzbuzz.c in Swift

--- swift/fizzbuzz.swift
+++ swift/fizzbuzz.swift
@@ -1,8 +1,16 @@
-for i in 1...10000 {
-    switch (i % 3, i % 5) {
-    case (0, 0): print("Fizzbuzz")
-    case (0, _): print("Fizz")
-    case (_, 0): print("Buzz")
-    default: print(i)
+func fizzbuzz(_ n: Int) {
+    for i in 1...n {
+        switch (i % 3, i % 5) {
+        case (0, 0): print("Fizzbuzz")
+        case (0, _): print("Fizz")
+        case (_, 0): print("Buzz")
+        default: print(i)
+        }
     }
+
 }
+
+guard let n = Int(CommandLine.arguments[1]) else {
+    fatalError("Could not parse integer from argument")
+}
+fizzbuzz(n)
