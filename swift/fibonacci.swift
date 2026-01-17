41e055338c3e5cd61e53530c387c69f7dd0d061c
noClaps <git@zerolimits.dev>
2026-01-17 00:06:32 +0000

    Rewrite fibonacci.ts in Swift

--- swift/fibonacci.swift
+++ swift/fibonacci.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 func fib(_ n: Int) -> [Int] {
     var nums: [Int] = []
 
@@ -12,5 +14,27 @@ func fib(_ n: Int) -> [Int] {
     return nums
 }
 
+/// This is a fast implentation of the Fibonacci sequence that uses repeated
+/// multiplication with the golden ratio to achieve higher performance.
+func fib2(_ n: Int) -> [Int] {
+    switch n {
+    case 0: return []
+    case 1: return [0]
+    case 2: return [0, 1]
+    default: break
+    }
+
+    var nums = [0, 1, 1]
+    let PHI = (1 + sqrt(5)) / 2
+
+    for _ in 2..<n - 1 {
+        nums.append(Int(round(Double(nums.last!) * PHI)))
+    }
+
+    return nums
+}
+
 let n = Int(CommandLine.arguments[1])!
-print(fib(n))
+
+print("fib:", fib(n))
+print("fib2:", fib2(n))
