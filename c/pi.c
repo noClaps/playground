17585ef3441eef82da5f3a697ec0aecdfad86200
noClaps <git@zerolimits.dev>
2026-05-23 12:58:43 +0100

    c,swift: Rewrite pi in C

---
+++ c/pi.c
@@ -0,0 +1,7 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+  printf("%.20lf\n", M_PI);
+  printf("%.20lf\n", atan(1) * 4);
+}
--- swift/pi.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-
-print(String(format: "%.20f", Double.pi))
-print(String(format: "%.20f", atan(1.0) * 4))
